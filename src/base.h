e607df56f3e96b3374865734e4041aaa6d520fe8
LeavingKnife <2826225037@qq.com>
2024-07-25 15:31:20 +0800

    Add base module

---
+++ src/base.h
@@ -0,0 +1,11 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <iostream>
+
+
+namespace BASE
+{
+}
+
+#endif
--- src/data.cpp
+++ src/data.cpp
@@ -4,6 +4,7 @@
 #include<fstream>
 
 #include"sha.h"
+#include"base.h"
 
 namespace DATA{
 
